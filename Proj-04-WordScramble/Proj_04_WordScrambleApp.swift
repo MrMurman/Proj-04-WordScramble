57bb0d63b61b1fc97492995f8c0c27b9e505b5ee
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-15 14:57:12 +0300

    Initial Commit

---
+++ Proj-04-WordScramble/Proj_04_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proj_04_WordScrambleApp.swift
+//  Proj-04-WordScramble
+//
+//  Created by Андрей Бородкин on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Proj_04_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
